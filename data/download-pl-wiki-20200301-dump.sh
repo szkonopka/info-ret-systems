635775d5bac7763cba8637f0f33643da4a9da5a6
szkonopka <szykonopka@gmail.com>
2020-03-28 00:23:56 +0100

    Download multiple files for measurements - change the script

--- data/download-pl-wiki-20200301-dump.sh
+++ data/download-pl-wiki-20200301-dump.sh
@@ -1,4 +1,19 @@
-#!/bin/sh
+#!/bin/bash
 
-wget "https://dumps.wikimedia.org/plwiki/20200301/plwiki-20200301-pages-articles-multistream.xml.bz2" -O plwiki-20200301-articles.xml.bz2
-bzip2 -d plwiki-20200301-articles.xml.bz2
+declare -a plwikifiles=(
+    "plwiki-20200301-pages-articles-multistream1.xml-p1p169750.bz2"
+    "plwiki-20200301-pages-articles-multistream2.xml-p169751p510662.bz2"
+    "plwiki-20200301-pages-articles-multistream3.xml-p510663p1056310.bz2"
+    "plwiki-20200301-pages-articles-multistream4.xml-p1056311p1831508.bz2"
+    "plwiki-20200301-pages-articles-multistream5.xml-p1831509p3070393.bz2"
+    "plwiki-20200301-pages-articles-multistream6.xml-p3070394p4570393.bz2"
+    "plwiki-20200301-pages-articles-multistream6.xml-p4570394p4727706.bz2"
+)
+
+for index in ${!plwikifiles[@]}
+do
+    fileid=$[index+1]
+    echo "Downloading plwikifiles - ${fileid}. ${plwikifiles[$index]}"
+    wget "https://dumps.wikimedia.org/plwiki/20200301/${plwikifiles[$index]}" -O plwiki-20200301-articles-${fileid}.xml.bz2
+    bzip2 -d plwiki-20200301-articles-${fileid}.xml.bz2
+done
